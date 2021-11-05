58be94a67b9c1acb6520f7990027a8285a82faf9
Byungchul Park <byungchul.park@lge.com>
2021-11-05 19:10:36 +0900

    dept: Implement Dept(Dependency Tracker)
    CURRENT STATUS
    --------------
    Lockdep tracks acquisition order of locks in order to detect deadlock,
    and IRQ and IRQ enable/disable state as well to take accident
    acquisitions into account.
    
    Lockdep should be turned off once it detects and reports a deadlock
    since the data structure and algorithm are not reusable after detection
    because of the complex design.
    
    PROBLEM
    -------
    *Waits* and their *events* that never reach eventually cause deadlock.
    However, Lockdep is only interested in lock acquisition order, forcing
    to emulate lock acqusition even for just waits and events that have
    nothing to do with real lock.
    
    Even worse, no one likes Lockdep's false positive detection because that
    prevents further one that might be more valuable. That's why all the
    kernel developers are sensitive to Lockdep's false positive.
    
    Besides those, by tracking acquisition order, it cannot correctly deal
    with read lock and cross-event e.g. wait_for_completion()/complete() for
    deadlock detection. Lockdep is no longer a good tool for that purpose.
    
    SOLUTION
    --------
    Again, *waits* and their *events* that never reach eventually cause
    deadlock. The new solution, Dept(DEPendency Tracker), focuses on waits
    and events themselves. Dept tracks waits and events and report it if
    any event would be never reachable.
    
    Dept does:
       . Works with read lock in the right way.
       . Works with any wait and event e.i. cross-event.
       . Continue to work even after reporting multiple times.
       . Provides simple and intuitive APIs.
       . Does exactly what dependency checker should do.
    
    Q & A
    -----
    Q. Is this the first try ever to address the problem?
    A. No. Cross-release feature (b09be676e0ff2 locking/lockdep: Implement
       the 'crossrelease' feature) addressed it 2 years ago that was a
       Lockdep extention and merged but reverted shortly because:
    
       Cross-release started to report valuable hidden problems but started
       to give report false positive reports as well. For sure, no one
       likes Lockdep's false positive reports since it makes Lockdep stop,
       preventing reporting further real problems.
    
    Q. Why not Dept was developed as an extention of Lockdep?
    A. Lockdep definitely includes all the efforts great developers have
       made for a long time so as to be quite stable enough. But I had to
       design and implement newly because of the following:
    
       1) Lockdep was designed to track lock acquisition order. The APIs and
          implementation do not fit on wait-event model.
       2) Lockdep is turned off on detection including false positive. Which
          is terrible and prevents any extention for stronger detection from
          being developed.
    
    Q. Do you intend to totally replace Lockdep?
    A. No. Lockdep also checks if lock usage is correct. Of course, the
       deadlock check routine should be replaced but the other functions
       should be still there.
    
    Q. Do you mean the deadlock check routine should be replaced right away?
    A. No. I admit Lockdep is stable enough thanks to great efforts kernel
       developers have made. Lockdep and Dept, both should be in the kernel
       until Dept gets considered stable.
    
    Q. Stronger detection would give more false positive report. Which was a
       big problem when cross-release was introduced. Is it ok with Dept?
    A. It's ok. Dept allows multiple reporting thanks to simple and quite
       generalized design. Of course, false positive reports should be fixed
       but it's no longer critical problem.
    
    Signed-off-by: Byungchul Park <byungchul.park@lge.com>

---
+++ kernel/dependency/dept_hash.h
@@ -0,0 +1,11 @@
+/*
+ * HASH(id, bits)
+ *
+ * id  : Id for the object of struct dept_##id.
+ * bits: 1UL << bits is the hash table size.
+ */
+
+HASH(dep	, 12)
+HASH(class	, 12)
+HASH(staleiw	,  4)
+HASH(staleie	,  4)
