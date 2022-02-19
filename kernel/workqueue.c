72050ca6c5c2c635ef5306c7e9e6c43163491415
Byungchul Park <byungchul.park@lge.com>
2022-02-19 15:39:54 +0900

    dept: Distinguish each work from another
    Workqueue already provides concurrency control. By that, any wait in a
    work doesn't prevents events in other works with the control enabled.
    Thus, each work would better be considered a different context.
    
    So let Dept assign a different context id to each work.
    
    Signed-off-by: Byungchul Park <byungchul.park@lge.com>

--- kernel/workqueue.c
+++ kernel/workqueue.c
@@ -50,6 +50,7 @@
 #include <linux/uaccess.h>
 #include <linux/sched/isolation.h>
 #include <linux/nmi.h>
+#include <linux/dept.h>
 
 #include "workqueue_internal.h"
 
@@ -2180,6 +2181,8 @@ __acquires(&pool->lock)
 
 	lockdep_copy_map(&lockdep_map, &work->lockdep_map);
 #endif
+	dept_work_enter();
+
 	/* ensure we're on the correct CPU */
 	WARN_ON_ONCE(!(pool->flags & POOL_DISASSOCIATED) &&
 		     raw_smp_processor_id() != pool->cpu);
