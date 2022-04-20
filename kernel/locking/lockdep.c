945e3e63c82b94516445922ba758913a5215743b
Byungchul Park <byungchul.park@lge.com>
2022-04-20 15:21:11 +0900

    dept: Implement Dept(Dependency Tracker)
    CURRENT STATUS
    --------------
    Lockdep tracks acquisition order of locks in order to detect deadlock,
    and IRQ and IRQ enable/disable state as well to take accident
    acquisitions into account.
    
    Lockdep should be turned off once it detects and reports a deadlock
    since the data structure and algorithm are not reusable after detection
    because of the complex design.
    
    PROBLEM
    -------
    *Waits* and their *events* that never reach eventually cause deadlock.
    However, Lockdep is only interested in lock acquisition order, forcing
    to emulate lock acqusition even for just waits and events that have
    nothing to do with real lock.
    
    Even worse, no one likes Lockdep's false positive detection because that
    prevents further one that might be more valuable. That's why all the
    kernel developers are sensitive to Lockdep's false positive.
    
    Besides those, by tracking acquisition order, it cannot correctly deal
    with read lock and cross-event e.g. wait_for_completion()/complete() for
    deadlock detection. Lockdep is no longer a good tool for that purpose.
    
    SOLUTION
    --------
    Again, *waits* and their *events* that never reach eventually cause
    deadlock. The new solution, Dept(DEPendency Tracker), focuses on waits
    and events themselves. Dept tracks waits and events and report it if
    any event would be never reachable.
    
    Dept does:
       . Works with read lock in the right way.
       . Works with any wait and event e.i. cross-event.
       . Continue to work even after reporting multiple times.
       . Provides simple and intuitive APIs.
       . Does exactly what dependency checker should do.
    
    Q & A
    -----
    Q. Is this the first try ever to address the problem?
    A. No. Cross-release feature (b09be676e0ff2 locking/lockdep: Implement
       the 'crossrelease' feature) addressed it 2 years ago that was a
       Lockdep extension and merged but reverted shortly because:
    
       Cross-release started to report valuable hidden problems but started
       to give report false positive reports as well. For sure, no one
       likes Lockdep's false positive reports since it makes Lockdep stop,
       preventing reporting further real problems.
    
    Q. Why not Dept was developed as an extension of Lockdep?
    A. Lockdep definitely includes all the efforts great developers have
       made for a long time so as to be quite stable enough. But I had to
       design and implement newly because of the following:
    
       1) Lockdep was designed to track lock acquisition order. The APIs and
          implementation do not fit on wait-event model.
       2) Lockdep is turned off on detection including false positive. Which
          is terrible and prevents developing any extension for stronger
          detection.
    
    Q. Do you intend to totally replace Lockdep?
    A. No. Lockdep also checks if lock usage is correct. Of course, the
       dependency check routine should be replaced but the other functions
       should be still there.
    
    Q. Do you mean the dependency check routine should be replaced right
       away?
    A. No. I admit Lockdep is stable enough thanks to great efforts kernel
       developers have made. Lockdep and Dept, both should be in the kernel
       until Dept gets considered stable.
    
    Q. Stronger detection capability would give more false positive report.
       Which was a big problem when cross-release was introduced. Is it ok
       with Dept?
    A. It's ok. Dept allows multiple reporting thanks to simple and quite
       generalized design. Of course, false positive reports should be fixed
       anyway but it's no longer as a critical problem as it was.
    
    Signed-off-by: Byungchul Park <byungchul.park@lge.com>

--- kernel/locking/lockdep.c
+++ kernel/locking/lockdep.c
@@ -1184,7 +1184,7 @@ static inline struct hlist_head *keyhashentry(const struct lock_class_key *key)
 }
 
 /* Register a dynamically allocated key. */
-void lockdep_register_key(struct lock_class_key *key)
+void __lockdep_register_key(struct lock_class_key *key)
 {
 	struct hlist_head *hash_head;
 	struct lock_class_key *k;
@@ -1207,7 +1207,7 @@ out_unlock:
 restore_irqs:
 	raw_local_irq_restore(flags);
 }
-EXPORT_SYMBOL_GPL(lockdep_register_key);
+EXPORT_SYMBOL_GPL(__lockdep_register_key);
 
 /* Check whether a key has been registered as a dynamic key. */
 static bool is_dynamic_key(const struct lock_class_key *key)
@@ -4241,7 +4241,7 @@ static void __trace_hardirqs_on_caller(void)
  * stops watching. After the RCU transition lockdep_hardirqs_on() has to be
  * invoked to set the final state.
  */
-void lockdep_hardirqs_on_prepare(unsigned long ip)
+void __lockdep_hardirqs_on_prepare(unsigned long ip)
 {
 	if (unlikely(!debug_locks))
 		return;
@@ -4292,9 +4292,9 @@ void lockdep_hardirqs_on_prepare(unsigned long ip)
 	__trace_hardirqs_on_caller();
 	lockdep_recursion_finish();
 }
-EXPORT_SYMBOL_GPL(lockdep_hardirqs_on_prepare);
+EXPORT_SYMBOL_GPL(__lockdep_hardirqs_on_prepare);
 
-void noinstr lockdep_hardirqs_on(unsigned long ip)
+void noinstr __lockdep_hardirqs_on(unsigned long ip)
 {
 	struct irqtrace_events *trace = &current->irqtrace;
 
@@ -4356,12 +4356,12 @@ skip_checks:
 	trace->hardirq_enable_event = ++trace->irq_events;
 	debug_atomic_inc(hardirqs_on_events);
 }
-EXPORT_SYMBOL_GPL(lockdep_hardirqs_on);
+EXPORT_SYMBOL_GPL(__lockdep_hardirqs_on);
 
 /*
  * Hardirqs were disabled:
  */
-void noinstr lockdep_hardirqs_off(unsigned long ip)
+void noinstr __lockdep_hardirqs_off(unsigned long ip)
 {
 	if (unlikely(!debug_locks))
 		return;
@@ -4398,12 +4398,12 @@ void noinstr lockdep_hardirqs_off(unsigned long ip)
 		debug_atomic_inc(redundant_hardirqs_off);
 	}
 }
-EXPORT_SYMBOL_GPL(lockdep_hardirqs_off);
+EXPORT_SYMBOL_GPL(__lockdep_hardirqs_off);
 
 /*
  * Softirqs will be enabled:
  */
-void lockdep_softirqs_on(unsigned long ip)
+void __lockdep_softirqs_on(unsigned long ip)
 {
 	struct irqtrace_events *trace = &current->irqtrace;
 
@@ -4443,7 +4443,7 @@ void lockdep_softirqs_on(unsigned long ip)
 /*
  * Softirqs were disabled:
  */
-void lockdep_softirqs_off(unsigned long ip)
+void __lockdep_softirqs_off(unsigned long ip)
 {
 	if (unlikely(!lockdep_enabled()))
 		return;
@@ -4771,7 +4771,7 @@ static inline int check_wait_context(struct task_struct *curr,
 /*
  * Initialize a lock instance's lock-class mapping info:
  */
-void lockdep_init_map_type(struct lockdep_map *lock, const char *name,
+void __lockdep_init_map_type(struct lockdep_map *lock, const char *name,
 			    struct lock_class_key *key, int subclass,
 			    u8 inner, u8 outer, u8 lock_type)
 {
@@ -4831,7 +4831,7 @@ void lockdep_init_map_type(struct lockdep_map *lock, const char *name,
 		raw_local_irq_restore(flags);
 	}
 }
-EXPORT_SYMBOL_GPL(lockdep_init_map_type);
+EXPORT_SYMBOL_GPL(__lockdep_init_map_type);
 
 struct lock_class_key __lockdep_no_validate__;
 EXPORT_SYMBOL_GPL(__lockdep_no_validate__);
@@ -6291,7 +6291,7 @@ void lockdep_reset_lock(struct lockdep_map *lock)
 }
 
 /* Unregister a dynamically allocated key. */
-void lockdep_unregister_key(struct lock_class_key *key)
+void __lockdep_unregister_key(struct lock_class_key *key)
 {
 	struct hlist_head *hash_head = keyhashentry(key);
 	struct lock_class_key *k;
@@ -6326,7 +6326,7 @@ out_irq:
 	/* Wait until is_dynamic_key() has finished accessing k->hash_entry. */
 	synchronize_rcu();
 }
-EXPORT_SYMBOL_GPL(lockdep_unregister_key);
+EXPORT_SYMBOL_GPL(__lockdep_unregister_key);
 
 void __init lockdep_init(void)
 {
