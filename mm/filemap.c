c4952301ef57c5cab490c6d6070b73fc09f0b86c
Byungchul Park <byungchul.park@lge.com>
2022-12-22 10:17:52 +0900

    dept: Apply sdt_might_sleep_strong() to PG_{locked,writeback} wait
    Makes Dept able to track dependencies by PG_{locked,writeback} waits.
    
    Signed-off-by: Byungchul Park <byungchul.park@lge.com>

--- mm/filemap.c
+++ mm/filemap.c
@@ -42,6 +42,7 @@
 #include <linux/ramfs.h>
 #include <linux/page_idle.h>
 #include <linux/migrate.h>
+#include <linux/dept_sdt.h>
 #include <asm/pgalloc.h>
 #include <asm/tlbflush.h>
 #include "internal.h"
@@ -1215,6 +1216,9 @@ static inline bool folio_trylock_flag(struct folio *folio, int bit_nr,
 /* How many times do we accept lock stealing from under a waiter? */
 int sysctl_page_lock_unfairness = 5;
 
+static struct dept_map __maybe_unused PG_locked_map = DEPT_MAP_INITIALIZER(PG_locked_map, NULL);
+static struct dept_map __maybe_unused PG_writeback_map = DEPT_MAP_INITIALIZER(PG_writeback_map, NULL);
+
 static inline int folio_wait_bit_common(struct folio *folio, int bit_nr,
 		int state, enum behavior behavior)
 {
@@ -1225,6 +1229,11 @@ static inline int folio_wait_bit_common(struct folio *folio, int bit_nr,
 	bool thrashing = false;
 	unsigned long pflags;
 	bool in_thrashing;
+
+	if (bit_nr == PG_locked)
+		sdt_might_sleep_strong(&PG_locked_map);
+	else if (bit_nr == PG_writeback)
+		sdt_might_sleep_strong(&PG_writeback_map);
 
 	if (bit_nr == PG_locked &&
 	    !folio_test_uptodate(folio) && folio_test_workingset(folio)) {
@@ -1326,6 +1335,8 @@ repeat:
 	 * to do it here would be difficult and prone to races.
 	 */
 	finish_wait(q, wait);
+
+	sdt_might_sleep_finish();
 
 	if (thrashing) {
 		delayacct_thrashing_end(&in_thrashing);
