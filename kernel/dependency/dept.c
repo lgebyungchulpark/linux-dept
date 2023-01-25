8bc5e1e1cc225dba4fb254beaf5601f723405322
Byungchul Park <max.byungchul.park@gmail.com>
2023-01-25 19:39:31 +0900

    dept: Distinguish each work from another
    Workqueue already provides concurrency control. By that, any wait in a
    work doesn't prevents events in other works with the control enabled.
    Thus, each work would better be considered a different context.
    
    So let Dept assign a different context id to each work.
    
    Signed-off-by: Byungchul Park <max.byungchul.park@gmail.com>

--- kernel/dependency/dept.c
+++ kernel/dependency/dept.c
@@ -1977,6 +1977,16 @@ void dept_hardirqs_off_ip(unsigned long ip)
 }
 EXPORT_SYMBOL_GPL(dept_hardirqs_off_ip);
 
+/*
+ * Assign a different context id to each work.
+ */
+void dept_work_enter(void)
+{
+	struct dept_task *dt = dept_task();
+
+	dt->cxt_id[DEPT_CXT_PROCESS] += 1UL << DEPT_CXTS_NR;
+}
+
 void dept_kernel_enter(void)
 {
 	struct dept_task *dt = dept_task();
